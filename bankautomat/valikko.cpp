d7d8440887e537f16cfba1cd894e2830d251e4bc
Immu Saarijärvi <you@example.com>
2021-12-08 19:58:31 +0200

    nosta rahaa timer toimii oikein, pääkäyttöliittymän timer vaatii vielä pientä säätöä

--- bankautomat/valikko.cpp
+++ bankautomat/valikko.cpp
@@ -5,11 +5,19 @@ valikko::valikko(QString idTili, QString idKortti, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::valikko)
 {
+    timerCounter=0;
     this->idTili=idTili;
     this->idKortti=idKortti;
     ui->setupUi(this);
     qDebug()<<"valikko "+this->idTili;
 
+    objQTimer = new QTimer();
+
+    // setup signal and slot
+    connect(objQTimer, SIGNAL(timeout()),
+            this, SLOT(TimerSlot()));
+
+    objQTimer->start(1000);
 }
 
 
@@ -21,34 +29,58 @@ valikko::~valikko()
     objPankki=nullptr;
 }
 
+void valikko::TimerSlot()
+{
+    qDebug() << "Valikon Timer.."<<timerCounter;
+    timerCounter++;
+
+    if(timerCounter==30)
+        {
+            objQTimer->stop();
+
+            delete objQTimer;
+            objQTimer = nullptr;
+
+            this->close();
+            //emit finishProgram();
+        }
+}
+
 void valikko::on_btnValikkoClose_clicked()
 {
+    timerCounter=0;
     this->close();
 
 }
 
 void valikko::on_btnNostaRahaa_clicked()
 {
+    timerCounter=0;
+    objQTimer->stop();
     objPankki=new Pankki(this->idTili, this->idKortti);
     objPankki->show();
 }
 
 void valikko::on_btnNaytaSaldo_clicked()
 {
-
+    timerCounter=0;
+    objQTimer->stop();
 }
 
 void valikko::on_btnSelaaTilitapahtumia_clicked()
 {
-
+    timerCounter=0;
+    objQTimer->stop();
 }
 
 void valikko::on_btnKirjauduUlos_clicked()
 {
-
+    timerCounter=0;
+    objQTimer->stop();
 }
 
 void valikko::on_btnValitseTili_clicked()
 {
-
+    timerCounter=0;
+    objQTimer->stop();
 }
